47ed8d572381d39fd03ed34611d9262f73147732
Ferran Fusalba Roselló <ferranfusalba@gmail.com>
2024-07-13 18:23:35 +0200

    Create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "@tanstack/react-router";
-import Sidebar from "./components/sidebar/Sidebar";
 import { useCoreStore } from "./store/store";
+import Sidebar from "./components/sidebar/Sidebar";
+import Header from "./components/header/Header";
 
 function App() {
   const isSidebarExpanded = useCoreStore((state) => state.isSidebarExpanded);
 
   return (
     <>
+      <Header />
       <div className="main-app">
         <Sidebar />
         <main className={isSidebarExpanded ? "sidebar-expanded" : ""}>
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import { StyledHeader } from "./Header.styles";
+
+const Header = () => {
+  return (
+    <StyledHeader>
+      <div>UI Kit React</div>
+      <div>(user options)</div>
+    </StyledHeader>
+  );
+};
+
+export default Header;
